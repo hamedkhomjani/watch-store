d818c55d5ffae533d52bcb83f1db67b5dfb3b72d
Hamed Khomjani <eng.h.khomjani@gmail.com>
2020-05-16 16:04:22 +0430

    Men watch data added

--- watch-store-products.js
+++ watch-store-products.js
@@ -1,18 +1,218 @@
 let products = [{
-    name: "LE LOCLE POWERMATIC 80",
-    price: 5550.00,
+    name: "TISSOT GENTLEMAN",
+    gender: "Men",
+    price: 3300,
+    year: 2016,
+    images: [
+        "T127.410.11.041.00_1",
+        "t127.410.11.041.00_zoombracelet"
+    ],
+    case: {
+        color: "Silver",
+        material: "316L stainless steel case"
+    },
+    strap: {
+        color: "Grey",
+        material: "Stainless steel"
+    },
+    waranty: "2 Years of Warranty",
+    description: "The Tissot Gentleman is a multi-purpose watch, both ergonomic and elegant in any circumstance. It is equally suitable for wearing in a business environment, where conventional dress codes apply, as at the weekend, when it adapts easily to leisure activities. As part of the life of a modern, active man, the Tissot Gentleman becomes the perfect companion for every day, every occasion and every style."
+}, {
+    name: "TISSOT CHRONO XL NBA SPECIAL EDITION",
+    gender: "Men",
+    price: 7500,
+    images: [
+        "T116.617.36.051.12",
+        "T116.617.36.051.12_zoombracelet"
+    ],
+    case: {
+        color: "Black",
+        material: "316L stainless steel case with black PVD coating"
+    },
+    strap: {
+        color: "Orange",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "This very sporty and fashionable large chronograph (45mm) exclusively dedicated to NBA has a special caseback where the NBA logo is engraved and a leather strap which imitates a basket ball. We created the Chrono XL NBA for passionate Basketball fans looking for a Swiss chronograph at an affordable price without compromising on quality and style."
+}, {
+    name: "TISSOT LE LOCLE POWERMATIC 80",
+    gender: "Men",
+    price: 5500,
     images: [
         "T006.407.16.033.00_7",
         "T006.407.16.033.00_zoombracelet_1"
     ],
     case: {
         color: "Silver",
-        material: "stainless steel"
+        material: "316L stainless steel case"
     },
     strap: {
         color: "Black",
         material: "Leather"
     },
     waranty: "2 Years of Warranty",
-    description: "The name Le Locle seems to be a reliable ingredient of success. As well as being the name of Tissot's home and heritage, nestled in the Swiss Jura Mountains, it is the name of a hugely popular automatic watch family. The models display exquisite elegance with details such as Roman numerals and a traditional Le Locle signature which complete the picture of classical chic."
+    description: "The name Le Locle seems to be a reliable ingredient of success. As well as being the name of Tissot's home and heritage, nestled in the Swiss Jura Mountains, it is the name of a hugely popular automatic watch family. The models display exquisite elegance with details such as Roman numerals and a traditional Le Locle signature which complete the picture of classical chic. "
+}, {
+    name: "TISSOT GENTLEMAN POWERMATIC 80 SILICIUM",
+    gender: "Men",
+    price: 6500,
+    images: [
+        "T127.407.11.041.00_1",
+        "T127.407.11.041.00_zoombracelet_1"
+    ],
+    case: {
+        color: "Silver",
+        material: "316L stainless steel case"
+    },
+    strap: {
+        color: "Grey",
+        material: "Stainless steel"
+    },
+    waranty: "2 Years of Warranty",
+    description: "The name Le Locle seems to be a reliable ingredient of success. As well as being the name of Tissot's home and heritage, nestled in the Swiss Jura Mountains, it is the name of a hugely popular automatic watch family. The models display exquisite elegance with details such as Roman numerals and a traditional Le Locle signature which complete the picture of classical chic. "
+}, {
+    name: "TISSOT T-TOUCH EXPERT SOLAR II SWISS EDITION",
+    gender: "Men",
+    price: 7500,
+    images: [
+        "T110.420.46.051.00_1",
+        "T127.407.11.041.00_zoombracelet_1"
+    ],
+    case: {
+        color: "Brown",
+        material: "Antimagnetic titanium case"
+    },
+    strap: {
+        color: "Brown",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "This solar-powered watch features a touchscreen for access to many useful functions. This model comes in a fashionable color and has a unique and scratch-resistant ceramic bezel. The new ceramic bezel is scratch-resistant and brings a touch of color. The inscriptions on the bezel are in luminescent, which helps you to navigate even in the dark. Tissot’s pioneering spirit is what led to the creation of tactile watches in 1999. Today, it is the first to present a touch-screen watch powered by solar energy, confirming its position as leader in tactile technology in watchmaking. The rays of light on the dial allow to recharge the watch and to offer essential functions for everyday use. All this in a modern and dynamic stylish watch."
+}, {
+    name: "TISSOT CHRONO XL CLASSIC",
+    gender: "Men",
+    price: 4500,
+    images: [
+        "T116.617.16.047.00_1",
+        "T116.617.16.047.00_zoombracelet_1"
+    ],
+    case: {
+        color: "Silver",
+        material: "316L stainless steel case"
+    },
+    strap: {
+        color: "Brown",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "Larger than life, the Tissot Chrono XL is characterized by its big case that has a diameter of 45mm. The size of the dial makes time easily readable, with contemporary Arabic numerals at 12, 3, 6 and 9 o’clock. The chronograph function adds to its urban style. Available in multiple styles. Some may prefer the vintage touches of old school leather bracelets in various shades of brown with stylish stitches. Extra character is added with grey or black PVD coating on the case of certain watches. The dials vary depending on the model, from dark green, blue or black. Others may prefer the mat stainless steel bracelet and case for a more sophisticated feel. Whichever one you choose; the Tissot Chrono XL will definitely make a statement. It’s time to think big with the Tissot Chrono XL!"
+}, {
+    name: "TISSOT LE LOCLE AUTOMATIQUE PETITE SECONDE",
+    gender: "Men",
+    price: 6000,
+    images: [
+        "T006.428.36.052.00",
+        "T006.428.36.052.00_zoombracelet"
+    ],
+    case: {
+        color: "Gold",
+        material: "316L stainless steel case with rose gold PVD coating"
+    },
+    strap: {
+        color: "Black",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "Named after Tissot's hometown, the cradle of the watchmaking industry, the Tissot Le Locle encases the brand DNA and craftsmanship values. The Le Locle Automatic Lady, powered by an automatic Swiss movement, displays exquisite elegance. Details such as Roman numerals and a traditional Le Locle signature complete the picture of classical chic. These finish off soave daytime and evening looks with a flourish."
+}, {
+    name: "TISSOT V8 ALPINE",
+    gender: "Men",
+    price: 5900,
+    images: [
+        "T106.417.16.201.01_1",
+        "T106.417.16.201.01_zoombracelet_1"
+    ],
+    case: {
+        color: "Silver",
+        material: "316L stainless steel case"
+    },
+    strap: {
+        color: "Black",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "This unique chronograph is inspired by 1960's race cars and celebrates the partnership between Tissot and Alpine. It's been designed to honor the original model and the new version of the Alpine car A110, by keeping the spirit of the 1960’s while adding some modernity thanks to the colours and materials we used. The beautiful carbon dial and the designed leather strap with holes remind of the racing spirit of this special edition."
+}, {
+    name: "TISSOT T-TOUCH EXPERT SOLAR II",
+    gender: "Men",
+    price: 7900,
+    images: [
+        "T110.420.44.051.00_1",
+        "T110.420.44.051.00_zoombracelet_1"
+    ],
+    case: {
+        color: "Grey",
+        material: "Antimagnetic titanium case with black PVD coating"
+    },
+    strap: {
+        color: "Grey",
+        material: "Titanium"
+    },
+    waranty: "2 Years of Warranty",
+    description: "This solar-powered watch features a touchscreen for access to many useful functions. This model comes in a fashionable color and has a unique and scratch-resistant ceramic bezel. The new ceramic bezel is scratch-resistant and brings a touch of color. The inscriptions on the bezel are in luminescent, which helps you to navigate even in the dark. Tissot’s pioneering spirit is what led to the creation of tactile watches in 1999. Today, it is the first to present a touch-screen watch powered by solar energy, confirming its position as leader in tactile technology in watchmaking. The rays of light on the dial allow to recharge the watch and to offer essential functions for everyday use. All this in a modern and dynamic stylish watch."
+}, {
+    name: "TISSOT GENTLEMAN",
+    gender: "Men",
+    price: 4900,
+    images: [
+        "T127.410.16.041.00",
+        "T127.410.16.041.00_zoombracelet"
+    ],
+    case: {
+        color: "Silver",
+        material: "316L stainless steel case"
+    },
+    strap: {
+        color: "Brown",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "The Tissot Gentleman is a multi-purpose watch, both ergonomic and elegant in any circumstance. It is equally suitable for wearing in a business environment, where conventional dress codes apply, as at the weekend, when it adapts easily to leisure activities. As part of the life of a modern, active man, the Tissot Gentleman becomes the perfect companion for every day, every occasion and every style."
+}, {
+    name: "TISSOT CHRONO XL",
+    gender: "Men",
+    price: 4900,
+    images: [
+        "T116.617.36.057.00_1",
+        "T116.617.36.057.00_zoombracelet_1"
+    ],
+    case: {
+        color: "Black",
+        material: "316L stainless steel case with black PVD coating"
+    },
+    strap: {
+        color: "Beige",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "This sporty chronograph offers one of the largest cases in the entire Tissot collection, as well as Swiss technology at an unbeatable price! The Tissot Chrono XL is one of the largest chronographs (45mm) in the Tissot collection."
+}, {
+    name: "TISSOT HERITAGE 1973",
+    gender: "Men",
+    price: 8000,
+    images: [
+        "T124.427.16.031.00_1",
+        "T124.427.16.031.00_MT_1" //in saat dar site ax band nadasht//
+    ],
+    case: {
+        color: "Silver",
+        material: "316L stainless steel case"
+    },
+    strap: {
+        color: "Black",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "As innovators by tradition, Tissot celebrates its historic ties to motor-racing with a re-issue of a seventies style chronograph. The Tissot Heritage 1973 proudly flaunts its membership of Tissot’s historic stable. It symbolizes the partnership that brings together Tissot and the Kessel Classics racing team. Meeting contemporary requirements, this 43 mm wide chronograph, with a silvery grey dial featuring matte black counters, is available in a limited edition of 1973 pieces. The overstitched black calfskin strap is a reproduction of the one Tissot had invented and patented in the 1960s, characterized by its large perforations. It alludes to pilots’ steering wheels of the time. To power this racing machine, Tissot has fitted a reliable, powerful and tested engine, the ETA Valjoux 7753 self-winding movement."
 }];
