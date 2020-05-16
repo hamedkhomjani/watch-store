8b51b436eb44c85672c7f3a0a0b85ea8da8a014c
hamedasemi <h-@live.se>
2020-05-16 11:04:27 +0200

    Add data model

---
+++ watch-store-products.js
@@ -0,0 +1,18 @@
+let products = [{
+    name: "LE LOCLE POWERMATIC 80",
+    price: 5550.00,
+    images: [
+        "T006.407.16.033.00_7",
+        "T006.407.16.033.00_zoombracelet_1"
+    ],
+    case: {
+        color: "Silver",
+        material: "stainless steel"
+    },
+    strap: {
+        color: "Black",
+        material: "Leather"
+    },
+    waranty: "2 Years of Warranty",
+    description: "The name Le Locle seems to be a reliable ingredient of success. As well as being the name of Tissot's home and heritage, nestled in the Swiss Jura Mountains, it is the name of a hugely popular automatic watch family. The models display exquisite elegance with details such as Roman numerals and a traditional Le Locle signature which complete the picture of classical chic."
+}];
