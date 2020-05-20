967e6164936493294ed4ec109410936a4b8beeca
hamedasemi <h-@live.se>
2020-05-20 21:33:55 +0200

    State

--- watch-store/watch-store.jsx
+++ watch-store/watch-store.jsx
@@ -2,16 +2,22 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class HelloMessage extends Component {
-    render() {
-      return (
-        <div>
-          Hello {this.props.name}
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = { date: new Date().toDateString() };
   }
-  
-  ReactDOM.render(
-    <HelloMessage name="Taylor" />,
-    document.getElementById('hello-example')
-  );
+
+  render() {
+    return (
+      <div>
+        Hello {this.props.name} Date: {this.state.date}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Hamed"></HelloMessage>,
+  document.getElementById('watch-store')
+);
