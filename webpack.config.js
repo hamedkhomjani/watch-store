4c9342b2af7a040874f6b15dbaa1214aec5e3a1a
hamedasemi <h-@live.se>
2020-05-20 11:16:54 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+import path from "path";
+
+import { optimize } from "webpack";
+
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    entry: {
+        app: './watch-store/watch-store.jsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "[chunkhash].js",
+        chunkFilename: "[chunkhash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }, {
+                test: /\.(css)$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // @ts-ignore
+        new optimize.AggressiveSplittingPlugin({
+            minSize: 30000,
+            maxSize: 50000
+        }),
+        new HtmlWebpackPlugin({
+            base: "/",
+            chunks: ['app'],
+            filename: 'index.html',
+            template: 'index.html'
+        }),
+    ],
+    devServer: {
+        host: "0.0.0.0",
+        useLocalIp: true,
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        port: 8080,
+        open: true,
+        historyApiFallback: true
+    }
+};
