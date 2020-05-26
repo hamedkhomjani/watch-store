b1fb8f67e8d4a2a5661867d25b16fd3496c42ac9
Hamed Khomjani <eng.h.khomjani@gmail.com>
2020-05-26 15:34:00 +0430

    shopping list

---
+++ watch-store/MyDate.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class MyDate extends Component {
+    render() {
+        return (<div>{this.props.date}</div>);
+    }
+}
--- watch-store/watch-store.jsx
+++ watch-store/watch-store.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { MyDate } from "./MyDate";
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          {this.props.list}
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 class HelloMessage extends Component {
 
-  state = { date: new Date().toDateString() };
+  state = { date: new Date().toDateString(), list: ["Instagaram", "WhatsApp", "Oculus"] };
 
   render() {
     return (
-      <div>Hello {this.props.name} Date: {this.state.date}</div>
+      <div>Hello {this.props.name} Date: <MyDate date={this.state.date} ></MyDate> list: <ShoppingList list={this.state.list}></ShoppingList></div>
     );
   }
 }
